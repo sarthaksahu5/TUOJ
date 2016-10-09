63de801f8b34fecccf60fe548d7dfa0b540cc514
Sarthak Sahu <vishalsahu5@gmail.com>
2016-10-09 11:59:14 +0530

    added 404 error handling feature

--- TUOJ.py
+++ TUOJ.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
     return render_template("main.html")
@@ -10,6 +11,12 @@ def homepage():
 @app.route('/dashboard/')
 def dashboard():
     return render_template("dashboard.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
 
 if __name__ == "__main__":
     app.run()
