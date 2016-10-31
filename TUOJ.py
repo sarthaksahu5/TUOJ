bb4896deaf72867844b8b819440c0b4ea80decc4
Saurav Verma <sverma_bemba15@thapar.edu>
2016-10-31 16:17:11 +0530

    Added many new features.
    Page to display all the problems.
    User can now submit solutions in java.
    User can now view their profile.
    Admin can now upload problems and add sample input and output file, according to which user's code will be evaluated.

--- TUOJ.py
+++ TUOJ.py
@@ -1,18 +1,20 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for, send_from_directory
 from sqlalchemy.orm import sessionmaker
-from Project import Register, Base
+from models import Register, Problem, Base
 from sqlalchemy import create_engine
-import os
-import bcrypt
+import os, time, bcrypt
 from werkzeug import secure_filename
 
 engine = create_engine('sqlite:///C:\\Users\\Saurav Verma\\Downloads\\Compressed\\flaskSamples\\6-FlaskLogin\\tuoj.db')
 app = Flask(__name__)
 
+Session = sessionmaker(bind=engine)
+s = Session()
+
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(APP_ROOT, 'css/')
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'Input/')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'py', 'db'])
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'java', 'py'])
 app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
 
 @app.errorhandler(404)
@@ -25,7 +27,14 @@ def allowed_file(filename):
 
 @app.route('/problems')
 def problems():
-    return render_template('Problem.html', value = False)
+    if session.get('logged_in'):
+        value = True
+        user = session['user']
+    else:
+        value = False
+        user = None
+
+    return render_template('Problem.html',problems = s.query(Problem).all(), user = user, value = value)
 
 @app.route('/upload')
 def index():
@@ -34,9 +43,13 @@ def index():
     else:
         user = session['user']
         if user == 'admin' :
-            return render_template('upload.html')
+            return render_template('upload.html', user = 'admin', value = True)
         else:
             return render_template('error.html')
+
+@app.route('/profile/<username>')
+def profile(username):
+    return render_template('profile.html', user = session['user'], value = True, profile = s.query(Register).filter_by(user_name = session['user']).first())
 
 @app.route('/')
 def home():
@@ -75,9 +88,6 @@ def register():
     POST_EMAIL    = request.form['email']
     POST_PASSWORD = request.form['password']
 
-    Session = sessionmaker(bind=engine)
-    s = Session()
-
     register = Register(POST_USERNAME, POST_ROLLNO, POST_FNAME, POST_LNAME, POST_COLLEGE, POST_EMAIL, POST_PASSWORD.encode('utf-8'))
     s.add(register)
 
@@ -85,20 +95,106 @@ def register():
 
     return home()
 
-@app.route('/upload', methods=['POST'])
+'''@app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
+        filename = secure_filename(request.form['problem_id'])
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return redirect(url_for('uploaded_file', filename=filename))
     else:
         flash('file format not supported')
-        return url_for('index.html')
+        return url_for('index.html')'''
+
+@app.route('/uploads', methods=['GET', 'POST'])
+def upload():
+
+    if not session.get('logged_in'):
+        return render_template('error.html')
+
+    else:
+        if session['user'] == 'admin':
+            if request.method == 'GET':
+                return render_template('uploads.html', user = session['user'], value = True)
+            if request.method =='POST':
+                problem_id = request.form['id']
+                problem_name = request.form['problem_name']
+                difficulty = request.form['difficulty']
+                content = request.form['content']
+                tags = request.form['tags']
+
+                file = request.files['file']
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], request.form['problem_name']+'_input.txt'))
+
+                file = request.files['file1']
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], request.form['problem_name']+'_output.txt'))
+
+                problem = Problem(problem_id, problem_name, difficulty, content, tags)
+                s.add(problem)
+                s.commit()
+
+                return home()
+
+        else:
+            return home()
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/problem/<name>/', methods=['GET', 'POST'])
+def problem(name):
+    if request.method == 'GET':
+
+        if session.get('logged_in'):
+            value = True
+            user = session['user']
+        else:
+            value = False
+            user = None
+        return render_template('problems.html', problem = s.query(Problem).filter_by(problem_name = name).first(), value = value, user = user)
+
+    if request.method == 'POST':
+
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        os.chdir('C:\\Users\\Saurav Verma\\Downloads\\Compressed\\flaskSamples\\6-FlaskLogin\\Input')
+
+        a = os.system('javac Main.java')
+        print(a)
+        start_time = time.time()
+        b = os.system('java Main > wow.txt')
+        print(b)
+
+        '''file1 = name
+        os.system('java '+file1+' < wow.txt')'''
+
+        file2 = name+'_input.txt'
+
+        val = os.system('fc wow.txt ' +file2)
+
+        if val:
+            return home()
+        else:
+            return problems()
+
+'''@app.route('/profile/<username>/manage/', methods=['GET', 'POST'])
+def manage_profile(username):
+    if request.method == 'GET':
+        if session.get('logged_in'):
+            return render_template('manage_profile.html', value = True, user = session['user'])
+        else:
+            return home()
+    else:
+'''
+
 
 @app.route("/logout")
 def logout():
